045ad3691ad24aa1cf94fc397fc589110f4e43cb
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-15 20:28:01 -0300

    improved

--- VigenereCipher/UnknownKeyLengthQuiz.java
+++ VigenereCipher/UnknownKeyLengthQuiz.java
@@ -9,9 +9,9 @@ public class UnknownKeyLengthQuiz {
 	public static void main(String[] args) {
 		// Quiz 1
 		VigenereBreaker v = new VigenereBreaker();
-		FileResource fr = new FileResource("./src/assignment/week4/messages/secretmessage2.txt");
+		FileResource fr = new FileResource("/Users/Eduardo/Desktop/Courses/Duke/JavaCourse03/data_Vigenere/secretmessage1.txt");
 		String message = fr.asString();
-		FileResource fr2 = new FileResource("./src/assignment/week4/dictionaries/English");
+		FileResource fr2 = new FileResource("/Users/Eduardo/Desktop/Courses/Duke/JavaCourse03/data_Vigenere/dictionaries/English");
 		HashSet<String> dictionary = v.readDictionary(fr2);
 		String decrypt = v.breakForLanguage(message, dictionary);
 		
@@ -24,7 +24,7 @@ public class UnknownKeyLengthQuiz {
 		System.out.println("The first line of text is " + lines[0]);
 		
 		// Quiz 4
-		int [] key = v.tryKeyLength(message, 38, 'e');
+		int [] key = v.tryKeyLength(message, 4, 'e');
 		VigenereCipher vc = new VigenereCipher(key);
     	decrypt = vc.decrypt(message);
     	count = v.countWords(decrypt, dictionary);
